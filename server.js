7a371e1e7162217065f69744f19849b287cdcceb
Andreas <AndreasPapaz@gmail.com>
2017-10-24 19:17:22 -0500

    basic setup for react

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+	type: 'application/vnd.api+json'
+}));
+app.use(express.static(__dirname + '/public/assets'));
+//Make Public Folder Static
+app.use(express.static('public'));
+
+const routes = requruire('./controllers/controller');
+app.use('/', routes);
+
+app.listen(PORT, () => {
+	console.log("BITFO : ", PORT);
+});
