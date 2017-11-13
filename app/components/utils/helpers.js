9bf2c273f8182570d22d82753c52db3834781ef2
Andreas <AndreasPapaz@gmail.com>
2017-11-12 23:25:25 -0600

    update

--- app/components/utils/helpers.js
+++ app/components/utils/helpers.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const Twitter = require('twitter');
 
 
-const T = new Twit({
+const T = new Twitter({
 	consumer_key: 'WLFFeOKBTfhyU4z0vgBJG3UGU',
 	consumer_secret: 'HK4B6YM3jDm0f442Q51YBbuNmAhHF4l6QQ7eh0FF3eyub7FvP7',
 	access_token: '830091872970346497-w9ghin4Hy3xUuZ2x0lVUe1frOReXezD',
@@ -17,9 +17,7 @@ const helper = {
 	},
 
 	some_test: function() {
-		T.get('/', { q: 'banana since:2011-07-11', count: 100 }, function(err, data, response) {
-			console.log(data);
-		})
+		client.get('statuses/user_timeline'm )
 	}
 
 }
